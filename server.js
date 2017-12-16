702fd1e280f2c560462b6dfd67f2a05820a7e4c4
scollins33 <sean_collins@gmx.com>
2017-12-15 20:22:36 -0500

    working on front end + express server working

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const expHandls = require('express-handlebars');
+const bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+const mongoose = require('mongoose');
+
+// port and app declaration
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// give app access to static public files
+app.use(express.static('public'));
+
+// body-parser and handlebars middleware
+app.use(bodyParser.json());
+app.engine('handlebars', expHandls({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// get the routes
+const routes = require('./controllers/routes');
+app.use('/', routes);
+
+// start the server listening
+app.listen(PORT, () => {
+    console.log(`Server is up and running on ${PORT}`);
+});
